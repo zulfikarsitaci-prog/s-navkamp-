2a9fba4108e8501c89f386fa5674fe1c92bbcc51
zulfikarsitaci-prog <zulfikar.sitaci@gmail.com>
2026-01-11 01:16:18 +0300

    Update app.py

--- app.py
+++ app.py
@@ -9,8 +9,7 @@ import random
 import database
 import base64
 import re
-from datetime 
-import datetime
+from datetime import datetime
 
 # --- AYARLAR ---
 st.set_page_config(page_title="Bağarası ÇPAL", page_icon="🎓", layout="wide", initial_sidebar_state="expanded")
